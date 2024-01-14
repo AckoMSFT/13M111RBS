f53cadfac06e1f85b7a60404338d1dfb27374f47
AckoMSFT <aleksandar.ivanovic.94@gmail.com>
2024-01-14 16:34:49 +0100

    Add required roles and permissions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,13 +2,13 @@ insert into users(id, username, password)
 values (1, 'bruce', 'wayne'),
        (2, 'peter', 'security_rules'),
        (3, 'tom', 'guessmeifyoucan'),
-        (4, 'santa', 'clause');
+       (4, 'santa', 'clause');
 
 insert into persons(id, firstName, lastName, email)
 values (1, 'bruce', 'wayne', 'notBatman@gmail.com'),
        (2, 'Peter', 'Petigrew', 'oneFingernailFewerToClean@gmail.com'),
        (3, 'Tom', 'Riddle', 'theyGotMyNose@gmail.com'),
-        (4, 'Santa', 'Clause', 'st@northPole.com');
+       (4, 'Santa', 'Clause', 'st@northPole.com');
 
 insert into gift(id, name, description, price)
 values (1, 'Christmas tree decoration', 'Decoration for a Christmas tree', 500.01),
@@ -46,9 +46,54 @@ values (1, 1, 'Very nice.');
 
 insert into roles(id, name)
 values (1, 'ADMIN'),
-       (2, 'MANAGER');
+       (2, 'MANAGER'),
+       (3, 'BUYER');
+
+-- santa (4) has role ADMIN (1)
+-- tom (3) has role MANAGER (2)
+-- bruce (1) and peter (2) have role BUYER (3)
 
 insert into user_to_roles(userId, roleId)
 values (4, 1),
-       (3, 2);
+       (3, 2),
+       (1, 3),
+       (2, 3);
+
+insert into permissions(id, name)
+values (1, 'ADD_COMMENT'),
+       (2, 'VIEW_GIFT_LIST'),
+       (3, 'CREATE_GIFT'),
+       (4, 'VIEW_PERSONS_LIST'),
+       (5, 'VIEW_PERSON'),
+       (6, 'UPDATE_PERSON'),
+       (7, 'VIEW_MY_PROFILE'),
+       (8, 'RATE_GIFT'),
+       (9, 'BUY_GIFT');
+
+-- admin (1) has all permissions
+-- manager (2) doesn't have (5) and (8)
+-- buyer (3) doesn't have (3), (4) and (5)
+insert into role_to_permissions(roleId, permissionId)
+values (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6),
+       (1, 7),
+       (1, 8),
+       (1, 9),
+       (2, 1),
+       (2, 2),
+       (2, 3),
+       (2, 4),
+       (2, 6),
+       (2, 7),
+       (2, 9),
+       (3, 1),
+       (3, 2),
+       (3, 6),
+       (3, 7),
+       (3, 8),
+       (3, 9);
 
